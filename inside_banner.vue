a34a13a1587ee62befa6020769a87b34a5e18463
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-05-29 18:05:16 -0400

    Terra Vista Village 1.0  /inside_banner.vue

--- inside_banner.vue
+++ inside_banner.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="inside_header_background" v-if="pageBanner" :style="{ background: 'linear-gradient(0deg, rgba(215, 78, 34, 0.8), rgba(245, 111, 68, 0.8)), url(' + pageBanner.image_url + ') center center no-repeat rgba(215, 78, 34)' }">
+    <div class="inside_header_background" v-if="pageBanner" :style="{ background: 'linear-gradient(0deg, rgba(215, 78, 34, 0.8), rgba(215, 78, 34, 0.8)), url(' + pageBanner.image_url + ') center center no-repeat rgba(215, 78, 34)' }">
         <div class="main_container">
             <div class="page_container">
                 <h2 v-html="pageName"></h2>
