64cc8d9f7bfb78320e94af9989e1a3e71f6fecd4
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-03-25 23:32:58 -0400

    Terra Vista Village 1.0  /default.vue

--- default.vue
+++ default.vue
@@ -1,24 +1,37 @@
 <template>
     <div>
+        <vue-headful
+            :title="title"
+            description="Static description"
+        />
         <h1 class="accessibility">{{ property.name }}</h1>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
-    define(["Vue", "vuex", "vue-meta"], function(Vue, Vuex, Meta) {
+    define(["Vue", "vuex",  "vue-headful"], function(Vue, Vuex,  vueHeadful) {
         return Vue.component("default-component", {
             template: template, // the variable template will be injected
             data: function() {
                 return {
-                    meta: {
-                       meta_title: "",
-                       meta_description: "",
-                       meta_keywords: "",
-                       meta_image: ""
-                    }
+                    // meta: {
+                    //   meta_title: "",
+                    //   meta_description: "",
+                    //   meta_keywords: "",
+                    //   meta_image: ""
+                    // }
+                    title: 'Dynamic title',
                 }
             },
+            
+            mounted() {
+                // dummy async operation to show watcher on properties
+                setTimeout(() => {
+                    this.title = 'Dynamic async title';
+                }, 3000);
+            },
+            
             beforeRouteEnter (to, from, next) {
                 next(vm => {
                     // access to component instance via `vm`
@@ -37,18 +50,18 @@
                     return this.$store.getters.findMetaDataByPath;
                 }
             },
-            metaInfo () {
-               return {
-                  title: this.meta.meta_title,
-                  meta: [
-                     { name: 'description', vmid: 'description', content: this.meta.meta_description },
-                     { name: 'keywords',  vmid: 'keywords', content: this.meta.meta_keywords },
-                     { property: 'og:title', vmid: 'og:title', content: this.meta.meta_title },
-                     { property: 'og:description', vmid: 'og:description', content: this.meta.meta_description },
-                     { property: 'og:image', vmid: 'og:image', content: this.meta.meta_image }
-                  ]
-               }
-            }
+            // metaInfo () {
+            //   return {
+            //       title: this.meta.meta_title,
+            //       meta: [
+            //          { name: 'description', vmid: 'description', content: this.meta.meta_description },
+            //          { name: 'keywords',  vmid: 'keywords', content: this.meta.meta_keywords },
+            //          { property: 'og:title', vmid: 'og:title', content: this.meta.meta_title },
+            //          { property: 'og:description', vmid: 'og:description', content: this.meta.meta_description },
+            //          { property: 'og:image', vmid: 'og:image', content: this.meta.meta_image }
+            //       ]
+            //   }
+            // }
         });
     });
 </script>
