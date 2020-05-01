418549ded8c4d886ec8dd21f5452935992745ee1
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-05-01 17:24:20 -0400

    Terra Vista Village 1.0  /default.vue

--- default.vue
+++ default.vue
@@ -15,24 +15,24 @@
             template: template, // the variable template will be injected
             data: function() {
                 return {
-                    // meta: {
-                    //   meta_title: "",
-                    //   meta_description: "",
-                    //   meta_keywords: "",
-                    //   meta_image: ""
-                    // }
+                    meta: {
+                      meta_title: "",
+                      meta_description: "",
+                      meta_keywords: "",
+                      meta_image: ""
+                    }
                     
                     // headful
-                    title: 'Dynamic title',
+                    // title: 'Dynamic title',
                 }
             },
             // headful
-            mounted() {
-                // dummy async operation to show watcher on properties
-                setTimeout(() => {
-                    this.title = 'Dynamic async title';
-                }, 3000);
-            },
+            // mounted() {
+            //     // dummy async operation to show watcher on properties
+            //     setTimeout(() => {
+            //         this.title = 'Dynamic async title';
+            //     }, 3000);
+            // },
             
             beforeRouteEnter (to, from, next) {
                 next(vm => {
@@ -52,18 +52,18 @@
                     return this.$store.getters.findMetaDataByPath;
                 }
             },
-            // metaInfo () {
-            //   return {
-            //       title: this.meta.meta_title,
-            //       meta: [
-            //          { name: 'description', vmid: 'description', content: this.meta.meta_description },
-            //          { name: 'keywords',  vmid: 'keywords', content: this.meta.meta_keywords },
-            //          { property: 'og:title', vmid: 'og:title', content: this.meta.meta_title },
-            //          { property: 'og:description', vmid: 'og:description', content: this.meta.meta_description },
-            //          { property: 'og:image', vmid: 'og:image', content: this.meta.meta_image }
-            //       ]
-            //   }
-            // }
+            metaInfo () {
+              return {
+                  title: this.meta.meta_title,
+                  meta: [
+                     { name: 'description', vmid: 'description', content: this.meta.meta_description },
+                     { name: 'keywords',  vmid: 'keywords', content: this.meta.meta_keywords },
+                     { property: 'og:title', vmid: 'og:title', content: this.meta.meta_title },
+                     { property: 'og:description', vmid: 'og:description', content: this.meta.meta_description },
+                     { property: 'og:image', vmid: 'og:image', content: this.meta.meta_image }
+                  ]
+              }
+            }
         });
     });
 </script>
