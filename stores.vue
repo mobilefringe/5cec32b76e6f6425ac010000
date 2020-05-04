40e8a746383fe7e5d868bae7dfc9d6dd56d8bc56
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2020-05-04 18:28:08 -0400

    Terra Vista Village 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -90,24 +90,7 @@
         return Vue.component("stores-component", {
             template: template, // the variable template will be injected
             props:['inside_banner'],
-            head: {
-                // To use "this" in the component, it is necessary to return the object through a function
-                title: function () {
-                  return {
-                    inner: this.meta.meta_title,
-                    separator: ' ', // Leave empty separator
-                    complement: ' ' // Leave empty complement
-                  }
-                },
-                meta: function () {
-                  return [
-                     { name: 'description', id: 'description', content: this.meta.meta_description },
-                     { name: 'keywords',  id: 'keywords', content: this.meta.meta_keywords },
-                     { property: 'og:title', id: 'og:title', content: this.meta.meta_title },
-                     { property: 'og:description', id: 'og:description', content: this.meta.meta_description }
-                  ]
-                }
-            },
+            
             data: function() {
                 return {
                     dataLoaded: false,
