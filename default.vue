99a20b57abeb1ed771e33261e11aaa45cced53d4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2020-05-04 18:28:42 -0400

    Terra Vista Village 1.0  /default.vue

--- default.vue
+++ default.vue
@@ -11,7 +11,7 @@
 
 <script>
     // define(["Vue", "vuex",  "vue-headful"], function(Vue, Vuex,  vueHeadful) {
-    define(["Vue", "vuex", "vue-meta"], function(Vue, Vuex, Meta) {
+    define(["Vue", "vuex"], function(Vue, Vuex) {
         return Vue.component("default-component", {
             template: template, // the variable template will be injected
             head: {
